cc428a0d2298145c1886cd686feb6d157a30699b
João Fernandes <joaofernandes30710@gmail.com>
2024-05-10 19:21:00 +0100

    testing preview lists

--- leaf/lists/models.py
+++ leaf/lists/models.py
@@ -10,6 +10,7 @@ import pandas as pd
 import werkzeug.utils
 
 from leaf.template_editor.models import *
+from flask import current_app
 
 
 def get_lists_data(accountId: int, userId: str, isAdmin: str):
