704e5d7bda0c4d4cd348c7b41a2b80b11a0292ee
João Fernandes <joaofernandes30710@gmail.com>
2024-05-23 21:59:34 +0100

    test

--- leaf/workflow/models.py
+++ leaf/workflow/models.py
@@ -1471,7 +1471,6 @@ def gen_feed(mycursor, account_list, list_feed_path, list_name):
 
                 # Ensure every item has a GUID, falling back to a default message if none is found
                 if not guid_found:
-                    rss.remove(item_elem)
                     channel.remove(item_elem)
                 # if not guid_found:
                     # ET.SubElement(item_elem, "guid").text = "Unique identifier not found"
