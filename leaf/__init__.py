2f820f3d29545ec327ab8bbb5dd5b574b9f4d84e
João Fernandes <joaofernandes30710@gmail.com>
2024-05-24 12:03:45 +0100

    test

--- leaf/__init__.py
+++ leaf/__init__.py
@@ -102,14 +102,12 @@ def create_app(config_class=Config):
     app.register_blueprint(saml_route)
     app.register_blueprint(files_manager)
 
-    check_if_should_publish_items()
-    with app.app_context():
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(func=check_if_should_publish_items, trigger="interval", seconds=10)
-        scheduler.start()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=check_if_should_publish_items, trigger="interval", seconds=10)
+    scheduler.start()
 
-        # Shut down the scheduler when exiting the app
-        atexit.register(lambda: scheduler.shutdown())
+    # Shut down the scheduler when exiting the app
+    atexit.register(lambda: scheduler.shutdown())
 
     # Check Database Integrity
     check_db()
