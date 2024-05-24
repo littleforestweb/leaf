96c9df6e704d5fa63f7ad56940dcae81fc74aa59
João Fernandes <joaofernandes30710@gmail.com>
2024-05-24 11:58:03 +0100

    test

--- run.py
+++ run.py
@@ -4,9 +4,15 @@
 from flask_cors import CORS
 
 from leaf import create_app, Config
+import logging
 
 app = create_app()
 CORS(app, origins=Config.CORS_ALLOWED_ORIGINS)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+    app.run()
