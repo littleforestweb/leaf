fa15ec0619ebe1fb5478ffbc1de6052ac8315072
João Fernandes <joaofernandes30710@gmail.com>
2024-05-24 12:02:15 +0100

    test

--- leaf/__init__.py
+++ leaf/__init__.py
@@ -102,6 +102,7 @@ def create_app(config_class=Config):
     app.register_blueprint(saml_route)
     app.register_blueprint(files_manager)
 
+    check_if_should_publish_items()
     with app.app_context():
         scheduler = BackgroundScheduler()
         scheduler.add_job(func=check_if_should_publish_items, trigger="interval", seconds=10)
