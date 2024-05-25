e0e0c3b8b736976f7176ca307d9ac25eb721ac3d
João Fernandes <joaofernandes30710@gmail.com>
2024-05-25 03:16:28 +0100

    log clean

--- leaf/lists/models.py
+++ leaf/lists/models.py
@@ -2013,8 +2013,10 @@ def trigger_new_scrape(request):
             mydb.commit()
 
             count_pages = 0
+                
             for folder in folders_to_scrape:
                 folder_path = os.path.join(Config.WEBSERVER_FOLDER, folder)
+                current_app.logger.debug(f"Folder: {folder_path}")
                 # Check if the folder exists
                 if os.path.exists(folder_path):
                     print(f"Folder exist: {folder_path}")
