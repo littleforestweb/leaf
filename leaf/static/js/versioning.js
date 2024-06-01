398b87558b0b139ce364fdadc33cbcae3033e44c
xhico <francgf@gmail.com>
2024-06-01 01:57:25 +0100

    Update versioning.js

--- leaf/static/js/versioning.js
+++ leaf/static/js/versioning.js
@@ -154,6 +154,7 @@ window.addEventListener('DOMContentLoaded', async function main() {
             },
             {
                 aTargets: [2],
+                sClass: "truncate",
                 mData: function (source, type, val) {
                     return "<a class='green-link' href='#' onclick='open_file(\"" + file_id + "\", \"" + source["commit"] + "\")'>" + file_path + "</a>";
                 }
