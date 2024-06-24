c19f42d68a42aa26698c7c788ba919fe48bf1cec
João Fernandes <joaofernandes30710@gmail.com>
2024-06-24 13:47:19 +0100

    test

--- leaf/workflow/models.py
+++ leaf/workflow/models.py
@@ -1374,6 +1374,7 @@ def proceed_action_workflow(request, not_real_request=None):
                             with open(local_path, "w") as outFile:
                                 outFile.write(data)
 
+                            current_app.logger.debug(original_content_changed)
                             assets = find_page_assets(original_content_changed)
                             current_app.logger.debug("Testing assets: ")
                             current_app.logger.debug(assets)
